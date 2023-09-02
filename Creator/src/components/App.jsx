3cd313eeb2c9ac9ae3fa8087e74efeb5da535475
omar sayed <omarsayed4736@gmail.com>
2023-09-02 23:51:38 +0300

    complete home&letter

---
+++ Creator/src/components/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./PageStructure/Home";
+import FirstStep from "./LetterPages/FirstStep/FirstStep";
+import SecondStep from "./LetterPages/SecondStep/SecondStep";
+import FinalStep from "./LetterPages/FinalStep/FinalStep";
+import CompleteLetter from "./LetterModels/CompleteLetter";
+import CoverSelect from "./LetterModels/CoverSelect";
+import Letter from "./LetterModels/Letter";
+import FirstPage from "../pages/first.page/first-page";
+import SecondPage from "../pages/second.page/second-page";
+import ThirdPage from "../pages/third.page/third-page";
+import ResumePage from "../pages/resume.page/resume";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Contact" element={<FirstPage />} />
+            <Route path="/Education" element={<SecondPage />}></Route>
+            <Route path="/Skills" element={<ThirdPage />}></Route>
+            <Route path="/Letter" element={<Letter />} />
+            <Route path="/CoverSelect" element={<CoverSelect />} />
+            <Route path="/FirstStep" element={<FirstStep />} />
+            <Route path="/SecondStep" element={<SecondStep />}></Route>
+            <Route path="/FinalStep" element={<FinalStep />}></Route>
+            <Route path="/CompleteLetter" element={<CompleteLetter />} />
+            <Route path='/Resume' element={<ResumePage />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ Creator/src/pages/third.page/third-page.jsx
@@ -0,0 +1,179 @@
+import React from "react";
+import { useState } from "react";
+import "./CSS/third-page.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import { useNavigate, useLocation } from "react-router-dom";
+import Header from "../../components/PageStructure/Header";
+import Footer from "../../components/PageStructure/Footer";
+
+
+
+const SecondPage = () => {
+  const [Hobbies, setHob] = useState([]);
+  const [HobArr, setHobArr] = useState([]);
+  const [Hob, setiHob] = useState(0);
+
+  const [Programming, setProg] = useState([]);
+  const [ProgArr, setProgArr] = useState([]);
+  const [prog, setiprog] = useState(0);
+
+  const [languages, setLang] = useState([]);
+  const [LangArr, setLangArr] = useState([]);
+  const [lan, setiLan] = useState(0);
+
+  const navigate = useNavigate();
+  const NextHandler = () => {
+    navigate("/Resume", {
+      state: {
+        ...location.state,
+        hobbies: Hobbies,
+        skills: Programming,
+        languages: languages,
+      },
+    });
+  };
+  const location = useLocation();
+  const BackHandler = () => navigate("/Education");
+
+  const AddLang = () => {
+    setiLan(lan + 1);
+    setLang(prev => [...prev, ''])
+    setLangArr((prevprojects, index) => {
+      return [
+        ...prevprojects,
+        <div className="Projects">
+          <input
+            type="text"
+            id={lan}
+            placeholder="e.g. Arabic"
+            onChange={(e) => {
+              setLang((prev) => {
+                const value = e.target.value;
+                let key = parseInt(e.target.id);
+                if (key <= prev.length) { prev[key] = value; } else { prev = [...prev, value]; }
+                return prev;
+              });
+            }}
+          ></input>
+        </div>,
+      ];
+    });
+  };
+
+  const AddProg = () => {
+    setiprog(prog + 1);
+    setProg(prev => [...prev, ''])
+    setProgArr((prevprojects, index) => {
+      return [
+        ...prevprojects,
+
+        <div className="Projects">
+          <input
+            type="text"
+            id={prog}
+            placeholder="e.g. Web development"
+            onChange={(e) => {
+              setProg((prev) => {
+                const value = e.target.value;
+                let key = parseInt(e.target.id);
+                if (key <= prev.length) { prev[key] = value; } else { prev = [...prev, value]; }
+                return prev;
+              });
+            }}
+          ></input>
+        </div>,
+      ];
+    });
+  };
+
+  const AddHobbie = () => {
+    setiHob(Hob + 1);
+    setHob(prev => [...prev, ''])
+    setHobArr((prevprojects, index) => {
+      return [
+        ...prevprojects,
+
+        <div className="Projects">
+          <input
+            type="text"
+            id={Hob}
+            placeholder="e.g. Fishing"
+            onChange={(e) => {
+              setHob((prev) => {
+                const value = e.target.value;
+                let key = parseInt(e.target.id);
+                if (key <= prev.length) { prev[key] = value; } else { prev = [...prev, value]; }
+                return prev;
+              });
+            }}
+          ></input>
+        </div>,
+      ];
+    });
+  };
+
+  function debug() {
+    console.log(Programming);
+    console.log(Hobbies);
+    console.log(languages);
+  }
+  return (
+    <>
+      <Header />
+      <div id="thirdPage">
+        <Navbar props={2} />
+        <div className="Background">
+          <div className="mainWrapper">
+            <p>Skills</p>
+            <div className="inputWrapper">
+              <button onClick={AddProg} className="Probutton">
+                Add Programming Skill
+              </button>
+              <div className="projectWrapper">
+                {ProgArr.map((project) => {
+                  return <div>{project}</div>;
+                })}
+              </div>
+            </div>
+
+            <div className="inputWrapper">
+              <button onClick={AddLang} className="Probutton">
+                Add Langauge
+              </button>
+              <div className="projectWrapper">
+                {LangArr.map((project) => {
+                  return <div>{project}</div>;
+                })}
+              </div>
+            </div>
+
+            <div className="inputWrapper">
+              <button onClick={AddHobbie} className="Probutton">
+                Add Hobbie
+              </button>
+              <div className="projectWrapper">
+                {HobArr.map((project) => {
+                  return <div>{project}</div>;
+                })}
+              </div>
+            </div>
+
+            <div className="buttonsWrapper">
+              <button className="Back" onClick={BackHandler}>
+                Back
+              </button>
+              <button className="Next" type="submit" onClick={NextHandler}>
+                Next
+              </button>
+              <button onClick={debug}>Kapran</button>
+            </div>
+          </div>
+          <div className="BackgroundImg"></div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default SecondPage;
