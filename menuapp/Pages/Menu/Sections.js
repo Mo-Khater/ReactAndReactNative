2190449ae3b3c72100239ebdbbd99d510ae70fd6
Mohamed-Ashraf273 <ma2736666@gmail.com>
2023-09-02 18:08:47 +0300

    lastModification

--- menuapp/Pages/Menu/Sections.js
+++ menuapp/Pages/Menu/Sections.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, SafeAreaView, Text, Image, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Button, SafeAreaView, Text, Image, View, TouchableOpacity, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -8,42 +8,38 @@ const Stack = createNativeStackNavigator();
 export default function App({ menusections }) {
   const [isVisible, setIsVisible] = useState(true);
   const [btnprsd, setBtnprsd] = useState([]);
+
   let displayView = (
     <View style={styles.newhomecont}>
-      {
-        btnprsd.map((btnprsds) => { 
-            const imagePath  = btnprsds.image_url;
-          return (
-            <Text style={styles.mnutext} key={btnprsds.dish_name}>
-              <Image source={{ uri: imagePath }} style={styles.img} />{'\n'}
-              Dish Name: {btnprsds.dish_name}{'\n'}
-              Description: {btnprsds.description}{'\n'}
-              Price: {btnprsds.price}${'\n'}
-              Rating: {btnprsds.rating}{'\n'}
-            </Text>
-          );
-        })
-      }
+      <ScrollView>
+        {btnprsd.map((btnprsds) => (
+          <View style={styles.mnutext} key={btnprsds.dish_name}>
+            <Image style={styles.img} source={{ uri: btnprsds.image_url }} />
+            <Text style={styles.dishName}>Dish Name: {btnprsds.dish_name}</Text>
+            <Text style={styles.description}>Description: {btnprsds.description}</Text>
+            <Text style={styles.price}>Price: {btnprsds.price}$</Text>
+            <Text style={styles.rating}>Rating: {btnprsds.rating}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 
   return (
     <View style={styles.homecont}>
       {isVisible ? (
-        menusections.map((menusection) => {
-          return (
-            <TouchableOpacity
-              style={styles.buttons}
-              onPress={() => {
-                setIsVisible(false);
-                setBtnprsd(menusection.dishes);
-              }}
-              key={menusection.section_name}
-            >
-              <Text style={{ fontSize: 16, color: '#17202A', fontWeight: 'bold' }}>{menusection.section_name}</Text>
-            </TouchableOpacity>
-          );
-        })
+        menusections.map((menusection) => (
+          <TouchableOpacity
+            style={styles.buttons}
+            onPress={() => {
+              setIsVisible(false);
+              setBtnprsd(menusection.dishes);
+            }}
+            key={menusection.section_name}
+          >
+            <Text style={styles.sectionName}>{menusection.section_name}</Text>
+          </TouchableOpacity>
+        ))
       ) : (
         displayView
       )}
@@ -54,29 +50,39 @@ export default function App({ menusections }) {
 const styles = StyleSheet.create({
   mnutext: {
     borderRadius: 10,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
+    padding: 20,
     marginBottom: 10,
     backgroundColor: '#E59866',
-    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 16,
-    marginLeft: 5,
-    fontWeight: 'bold',
   },
   newhomecont: {
     flex: 1,
     backgroundColor: '#F5CBA7',
     paddingTop: 20,
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingBottom:20
+    paddingHorizontal: 20,
+    paddingBottom: 20,
   },
   img: {
-    width: 100,
-    height: 80,
-    alignSelf: 'center',
+    width: 160,
+    height: 120,
+    marginBottom: 10,
+  },
+  dishName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  price: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  rating: {
+    fontSize: 16,
+    marginBottom: 5,
   },
   homecont: {
     flex: 1,
@@ -90,14 +96,16 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     marginBottom: 10,
     backgroundColor: '#E59866',
-    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  sectionName: {
     fontSize: 16,
-    marginLeft: 5,
+    color: '#17202A',
+    fontWeight: 'bold',
+  },
+  scrollContainer: {
+    alignItems: 'center',
+    paddingVertical: 20,
   },
 });
-/**
-              <Image source={require()} style={styles.img} />{'\n'}
- * 
- */
